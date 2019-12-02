90c6ccd31308ae869033c271463a557990337b79
Adam Hawkins <ahawkins@togetherwork.com>
2019-12-02 15:31:05 -0500

    add basic authentication

---
+++ src/containers/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Bootstrap from 'react-bootstrap';
+import { Auth } from 'aws-amplify';
+import './Login.css';
+
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+        await Auth.signIn(email, password);
+        alert("Logged in");
+    } catch (e) {
+        alert(e.message);
+    }
+
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="email" bsSize="large">
+          <label>Email</label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="password" bsSize="large">
+          <label>Password</label>
+          <Form.Control
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </Form.Group>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
