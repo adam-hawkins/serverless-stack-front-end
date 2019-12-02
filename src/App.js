4127cf320c92ef16956c8c465bb64039dfd546f3
Adam Hawkins <ahawkins@togetherwork.com>
2019-12-02 14:52:47 -0500

    set up front-end client

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 
 function App(props) {
-  return (
-    <div className="App container">
-        <Navbar bg="light" expand="lg" collapseOnSelect>
-            <Navbar.Brand>
-              <Link to="/">Scratch</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar>
-      <Routes />
-    </div>
-  );
+    return (
+        <div className="App container">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+              <Navbar.Brand href="/">Scratch</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#features">Features</Nav.Link>
+                  <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Nav>
+                 <LinkContainer to="/signup">
+                  <Nav.Link>Sign up </Nav.Link>
+                 </LinkContainer>
+                 <LinkContainer to="/login">
+                  <Nav.Link>Log in</Nav.Link>
+                 </LinkContainer>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Routes />
+         </div>
+    );
 }
 
 export default App;
